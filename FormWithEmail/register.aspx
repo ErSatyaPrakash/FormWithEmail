dcda213def1d2f26e63c320ba3aaf775cc59a88a
Satya Prakash <satyaprakash9919346535@gmail.com>
2024-06-15 10:19:44 +0530

    Add project files.

---
+++ FormWithEmail/register.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="register.aspx.cs" Inherits="FormWithEmail.register" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Submit Form</title>
+    <style>
+        .textbox{
+            margin-bottom:10px;
+            
+        }
+        div{
+            margin:50px 500px;
+        }
+        input, textarea{
+            height:40px;
+            width:300px;
+        }
+        textarea{
+            resize:none;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <label for="txtName">Name:</label><br />
+            <asp:TextBox ID="txtName" runat="server" CssClass="textbox form-control"></asp:TextBox><br />
+
+            <label for="txtEmail">Email:</label><br />
+            <asp:TextBox ID="txtEmail" runat="server" CssClass="textbox form-control"></asp:TextBox><br />
+
+            <label for="txtMessage">Message:</label><br />
+            <asp:TextBox ID="txtMessage" runat="server" CssClass="textbox form-control" TextMode="MultiLine"></asp:TextBox><br />
+
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ FormWithEmail/register.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FormWithEmail
+{
+    public partial class register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string message = txtMessage.Text;
+
+            // Call the method to send the email
+            SendEmail(name, email, message);
+        }
+        private void SendEmail(string name, string email, string message)
+        {
+            try
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587, // Use 465 for SSL or 587 for TLS
+                    Credentials = new NetworkCredential("deepakmaurya0590@gmail.com", "newucqasbiqgnvqt"),
+                    EnableSsl = true, // Enable SSL or TLS
+                };
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("deepakmaurya0590@gmail.com"),
+                    Subject = "Form Submission",
+                    Body = $"Name: {name}<br/>Email: {email}<br/>Message: {message}",
+                    IsBodyHtml = true,
+                };
+                mailMessage.To.Add("satyaprakash9919346535@gmail.com");
+
+                //MailMessage mailMessage = new MailMessage();
+                //mailMessage.From = new MailAddress("satyaprakash9919346535@gmail.com","Satya TMT");
+                //mailMessage.To.Add("aksa.trainglemind@gmail.com");
+                //mailMessage.Subject = "Form Submission";
+                //mailMessage.Body = $"Name: {name}<br/>Email: {email}<br/>Message: {message}";
+                //mailMessage.IsBodyHtml = true;
+
+                //SmtpClient smtpClient = new SmtpClient("satyaprakash9919346535@gmail.com", 587);
+                //smtpClient.Credentials = new NetworkCredential("satyaprakash9919346535@gmail.com", "");
+                //smtpClient.EnableSsl = true;
+
+                // Send the email
+                smtpClient.Send(mailMessage);   
+                Response.Write("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                // Handle any errors
+                Response.Write("Error sending email: " + ex.Message);
+            }
+        }
+    }
+}
